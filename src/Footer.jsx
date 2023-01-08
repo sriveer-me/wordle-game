05b89c9e825b2760ff412ab5912b82c7cc25cfb1
Sriveer Neerukonda <sriveer@proton.me>
2023-01-08 12:51:22 +0530

    feat:add new link to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,11 @@ function Footer(props){
         <footer className={classes.footer}>
             <div className={classes['footer-internal']+' width-constrainer'}>
                 <h2>© 2022 sriveer neerukonda</h2>
-                <a href="https://veeru-portfolio.netlify.app/"><i className="bi bi-link-45deg"></i> Let's connect</a>
+                <div className={classes.links}>
+                    <a href="https://veeru-portfolio.netlify.app/"><i className="bi bi-link-45deg"></i> Let's connect</a>
+                    <a href="https://github.com/veeru-neerukonda/wordle-game"><i class="bi bi-git"></i> View Code</a>
+                </div>
+                
             </div>
         </footer>
     );
